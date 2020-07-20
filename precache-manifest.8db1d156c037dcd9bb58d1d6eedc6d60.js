93855910b775a250f7cc04458a861cdc8fd14992
Joseph bayad <jobayad66@gmail.com>
2020-07-20 14:51:42 +0100

    Updates

---
+++ precache-manifest.8db1d156c037dcd9bb58d1d6eedc6d60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa150647c0f3f5e36ede1bee96ac9c3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16f211d932d9466f5d12",
+    "url": "/static/css/main.4821725e.chunk.css"
+  },
+  {
+    "revision": "56e3e6adb3e93b378eb0",
+    "url": "/static/js/2.13234ce9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.13234ce9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16f211d932d9466f5d12",
+    "url": "/static/js/main.0fa38db0.chunk.js"
+  },
+  {
+    "revision": "1869c2305709983cfbe6",
+    "url": "/static/js/runtime-main.683a4df2.js"
+  }
+]);
