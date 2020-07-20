e08d36b2f3c91b027887315430cfda8ca3f60599
Joseph bayad <jobayad66@gmail.com>
2020-07-20 15:40:40 +0100

    Updates

---
+++ precache-manifest.3dbadcdde5491f9f97579f3d462f8107.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ec15fe53fe0464d1628e1c7d18acbce",
+    "url": "/lyrics-app/index.html"
+  },
+  {
+    "revision": "16f211d932d9466f5d12",
+    "url": "/lyrics-app/static/css/main.4821725e.chunk.css"
+  },
+  {
+    "revision": "56e3e6adb3e93b378eb0",
+    "url": "/lyrics-app/static/js/2.13234ce9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/lyrics-app/static/js/2.13234ce9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16f211d932d9466f5d12",
+    "url": "/lyrics-app/static/js/main.0fa38db0.chunk.js"
+  },
+  {
+    "revision": "d45eca0e691ef1a817b2",
+    "url": "/lyrics-app/static/js/runtime-main.afc485e8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-lyrics-app/precache-manifest.618322742e77b1da6d573e3afa771b46.js"
+  "/lyrics-app/precache-manifest.3dbadcdde5491f9f97579f3d462f8107.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-lyrics-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lyrics-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
